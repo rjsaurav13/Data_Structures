c94883eb097174f5a49abd61f66a0500b3e1f1c6
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2020-09-30 16:45:00 +0530

    online class

---
+++ dptr - Copy.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int var,*ptr,**pptr,var=3000;
+	ptr= &var;
+	pptr=&ptr;
+	printf("value of var:%d\n",var);
+	printf("value at *ptr:%d\n",*ptr);
+	printf("value at **pptr:%d\n",**pptr);
+	return 0;
+}
