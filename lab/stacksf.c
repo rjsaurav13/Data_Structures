1bb66c9840841ab67aca7eb57e3f4013e8d5f09b
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2020-10-03 22:27:38 +0530

    Create stacksf.c

---
+++ lab/stacksf.c
@@ -0,0 +1,34 @@
+<html>
+<head>
+<title></title>
+<style>
+</style>
+<body>
+
+<p><i><b>a<sup>2</sup></b></i></p><br><br>
+<h1>London</h1>
+<hr>
+
+<p>London is the capital city of England. It is the most populous city in the United Kingdom, with a metropolitan area of over 13 million inhabitants.</p
+<p>
+HTML is 
+<mark>
+FUN
+</mark>
+ to learn!
+</p>
+<p>
+My favorite color is 
+<del>
+blue
+</del>
+ red.
+</p>
+<a href="https://www.w3schools.com/html/default.asp">lol
+</a>
+<button onclick="document.location = 'https://pbs.twimg.com/profile_images/1195030361664716801/dwAiIUYB_400x400.jpg'">sexy boi</button>
+
+
+</body>
+</head>
+</html>
