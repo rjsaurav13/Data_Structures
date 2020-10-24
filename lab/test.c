c0f94bb4008acd8d0a7fc32fb0cc989e6bbd6a93
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2020-10-25 00:27:55 +0530

    Update test.c

--- lab/test.c
+++ lab/test.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-int main()
+int MAX = 3;
+int main ()
 {
-//int x=10;
-void *gp;
-char ch = 'a';
-//gp = &x;
-//printf("\n Generic pointer points to the integer value = %d", *(int*)gp);
-gp = &ch;
-printf("\n Generic pointer now points to the character= %c", *(char*)gp);
+int *ptr, num= 5;
+ptr = &num;
+printf("%d",++*ptr +6);
+ 
 return 0;
 }
