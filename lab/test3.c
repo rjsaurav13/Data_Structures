12ffee256c21afddf94e7921528937ebcd566afb
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2020-10-29 19:00:52 +0530

    Update test3.c

--- lab/test3.c
+++ lab/test3.c
@@ -1,13 +1,101 @@
 #include<stdio.h>
-int main(){
-int arr[]={1,2,3,4,5};
-int *ptr,*ptr1,i;
-ptr=arr;
-ptr1=arr+2;
-printf("%d",*ptr1);
-printf("\nArray is: ");
-for(i=0;i<5;i++)
-printf(" %d", *(arr+i));
-return 0;
+#include<stdlib.h>
+#define maxsize 5
+void insert();
+void delete();
+void display();
 
+int main ()
+{
+int a=-1,b=-1;
+int * front = &a, * rear = &b;
+int queue[maxsize];
+    int choice;
+    while(choice != 4)
+    {
+        printf("\n1)-.Insert an element\n2)-.Delete an element\n3)-.Display\n4)-.Exit\n");
+        printf("\nEnter your choice: ");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1:
+            insert(queue,&front,&rear);
+            break;
+            case 2:
+            delete(queue,&front,&rear);
+            break;
+            case 3:
+            display(queue,&front,&rear);
+            break;
+            case 4:
+            exit(0);
+            break;
+            default:
+            printf("\nEnter valid choice??\n");
+        }
+    }
+return 0;
+}
+void insert(int queue[],int *f,int *r)
+{
+    int item;
+    printf("\nEnter the element\n");
+    scanf("%d",&item);
+    if((*r+1)%maxsize == *f)
+    {
+        printf("\nOVERFLOW");
+        return;
+    }
+    else if(*f == -1 && *r == -1)
+    {
+        *f = 0;
+        *r = 0;
+    }
+    else if(*r == maxsize -1 && *f != 0)
+    {
+        *r = 0;
+    }
+    else
+    {
+        *r = (*r+1)%maxsize;
+    }
+    queue[*r] = item;
+    printf("\nValue inserted ");
+}
+void delete(int queue[],int *f,int *r)
+{
+    int item;
+    if(*f == -1 && *r == -1)
+    {
+        printf("\nUNDERFLOW\n");
+        return;
+    }
+    else if(*f == *r)
+    {
+        *f = -1;
+        *r = -1;
+    }
+    else if(*f == maxsize -1)
+        {
+            *f = 0;
+        }
+    else
+        *f = *f + 1;
+}
+
+void display(int queue[],int *f,int *r)
+{
+   int i;
+   if(*f == -1)
+      printf("\nCircular Queue is Empty!!!\n");
+   else
+   {
+    printf("Elements in the queue\n");
+     for(i=*f; i<*r; i++){
+     printf("%d ",queue[i]);
+     printf("%d \n",queue[i]);
+
+      }
+
+   }
 }
