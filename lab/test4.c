0f2bd92bf2cc1bc43a1b1c4638a38606c866ab78
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2020-11-03 22:10:58 +0530

    Update test4.c

--- lab/test4.c
+++ lab/test4.c
@@ -1,14 +1,51 @@
-#include <stdio.h>
-int MAX = 3;
-int main ()
-{
-int var[] = {10, 100, 200};
-int i, *ptr;
-ptr = var;
-for ( i = 0; i < MAX; i++) {
-printf("Address of var[%d] = %d\n", i, ptr );
-printf("Value of var[%d] = %d\n", i, *ptr );
-ptr++;
- }
+#include<stdio.h>
+#include<conio.h>
+#include <stdlib.h>
+#define max 4
+int rear=-1,front=0;
+int q[max];
+void insert(){
+    int num;
+if(rear==max-1){
+    printf("Queue full");
+}
+else{
+        scanf("%d",num);
+        rear=(rear+1)%max;
+        q[rear]=num;
+        front++;
+}
+}
+void display(){
+    if(front==0){
+       printf("Queue is empty:");
+       exit(0);
+    }
+    else{
+        for(int i=0;i<rear-1;i++){
+            printf("%d",q[i]);
+        }
+    }
+}
+int main(){
+int ch;
+printf("Enter your choice: \n");
+printf("1)-INSERT 2)-DELETE 3)-DISPLAY 4)-EXIT");
+scanf("%d",&ch);
+switch(ch) {
+case 1:
+    insert();
+    break;
+case 2:
+    printf(":");
+   // del();
+    break;
+case 3:
+    display();
+    break;
+case 4:
+    return;
+    printf("PLEASE CHOOSE CORRECT OPTION:-:");
+}
 return 0;
 }
