723b9389aff685b616d942ac63c4438c7ed95edd
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2020-11-03 22:10:47 +0530

    Update cirqueues.c

--- lab/cirqueues.c
+++ lab/cirqueues.c
@@ -2,50 +2,56 @@
 #include<conio.h>
 #include <stdlib.h>
 #define max 4
+
+
+/*void del(){
+    if(front==0){
+        printf("Queue is empty::\n");
+    }
+    else{
+        front--;
+    }
+}*/
+
+void main()
+{
+ int choice,num;
 int rear=-1,front=0;
 int q[max];
-void insert(){
-    int num;
+ for(;;)
+ {
+ printf("Enter your choice\n1 Insertion\n2 Deletion\n3 Display\n4 Exit\n");
+ scanf("%d",&choice);
+ switch(choice)
+ {
+ case 1:
 if(rear==max-1){
     printf("Queue full");
 }
 else{
-        scanf("%d",num);
-        rear=(rear+1)%max;
-        q[rear]=num;
-        front++;
+printf("Enter the element\n");
+ scanf("%d",&num);
+ if(front==-1){
+ rear=(rear+1)%max;
+ q[rear]=num;
+ front++;
+ }
+
 }
+ break;
+ case 2:
+     printf("h");
+     //del();
+ break;
+ case 3:
+
+            printf("%d",q[0]);
+            rear=(rear+1)%max;
+
+
+ break;
+ case 4:exit(0);
+ }
+ }
 }
-void display(){
-    if(front==0){
-       printf("Queue is empty:");
-       exit(0);
-    }
-    else{
-        for(int i=front;i<rear;i++){
-            printf("%d",q[i]);
-        }
-    }
-}
-int main(){
-int ch;
-printf("Enter your choice: \n");
-printf("1)-INSERT 2)-DELETE 3)-DISPLAY 4)-EXIT");
-scanf("%d",&ch);
-switch(ch) {
-case 1:
-    insert();
-    break;
-case 2:
-    printf(":");
-   // del();
-    break;
-case 3:
-    display();
-    break;
-case 4:
-    return;
-    printf("PLEASE CHOOSE CORRECT OPTION:-:");
-}
-return 0;
-}
+
