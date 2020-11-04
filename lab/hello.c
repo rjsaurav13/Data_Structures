559952478f3fe55f9b01f11966b43b9f98f943c6
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2020-11-04 23:49:28 +0530

    Create hello.c

---
+++ lab/hello.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main()
+
+{    int len,sum=0;    printf("Please enter the length of array:");    scanf("%d",len);
+    int  var[len];
+    int i, *ptr[len];
+    for ( i = 0; i < len; i++) {      ptr[i] = &var[i];   }
+   for ( i = 0; i < len; i++) {
+      scanf("%d",ptr[i]);
+   }
+
+   for ( i = 0; i < len; i++) {
+      printf("Value of var[%d] = %d\n", i,*ptr[i] );
+   }
+}
