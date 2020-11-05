bad10fbac65c3650d3238f823e5202750f586ab9
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2020-11-05 23:31:41 +0530

    Create singly.c

---
+++ lab/singly.c
@@ -0,0 +1,65 @@
+//Insert front, Insert position, Delete based on key, Display, Reverse lis
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<malloc.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+struct node *start = NULL;
+struct node *insfront(struct node *);
+struct node *inspos(struct node *);
+struct node *delkey(struct node *);
+struct node *display(struct node *);
+int main(){
+int ch;
+for(;;){
+    printf("Enter your choice :\n");
+    printf("1)-Front Insert, 2)-Position Insertion,3)-Delete By Key,4)-Display:\n");
+    scanf("%d",ch);
+    switch(ch){
+        case 1:
+            start=insfront(start);
+            break;
+        case 2:
+            start=inspos(start);
+            break;
+        case 3:
+            start=delkey(start);
+            break;
+        case 4:
+            start=display(start);
+            break;
+        case 5:
+            exit(0);
+
+    }
+}
+return 0;
+}
+
+struct node *insfront(struct node *start){
+struct node *new_node;
+int num;
+printf("\n Enter the number to insert ");
+scanf("%d",&num);
+new_node= (struct node *)malloc(sizeof(struct node));
+new_node ->data=num;
+new_node ->next=start;
+start=new_node;
+return start;
+};
+
+struct node *display(struct node *start)
+{
+struct node *ptr;
+ptr = start;
+while(ptr != NULL)
+{
+ printf("\t %d", ptr -> data);
+ ptr = ptr -> next;
+}
+return start;
+}
