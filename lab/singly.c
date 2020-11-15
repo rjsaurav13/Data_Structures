5571a7299a96cafe2d4f2ec94dced132c12b87a0
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2020-11-15 23:13:54 +0530

    Update singly.c

--- lab/singly.c
+++ lab/singly.c
@@ -1,80 +1,140 @@
-//Insert front, Insert position, Delete based on key, Display, Reverse lis
 #include<stdio.h>
 #include<stdlib.h>
-#include<conio.h>
-#include<malloc.h>
 struct node
 {
-    int data;
-    struct node *next;
+int info;
+struct node *link;
 };
-struct node *start = NULL;
-struct node *insfront(struct node *);
-struct node *inspos(struct node *);
-struct node *delkey(struct node *);
-struct node *display(struct node *);
-struct node *revdisplay(struct node *);
-int main(){
-int ch;
-for(;;){
-    printf("Enter your choice :\n");
-    printf("1)-Front Insert, 2)-Position Insertion,3)-Delete By Key,4)-Display,5)-Reverse Display:\n");
-    scanf("%d",ch);
-    switch(ch){
-        case 1:
-            start=insfront(start);
-            break;
-        case 2:
-            start=inspos(start);
-            break;
-        case 3:
-            start=delkey(start);
-            break;
-        case 4:
-            start=display(start);
-            break;
-        case 5:
-            start=revdisplay(start);
-            break;
-        case 6:
-            exit(0);
-
-    }
-}
-return 0;
-}
-
-struct node *insfront(struct node *start){
-struct node *new_node;
-int num;
-printf("\n Enter the number to insert ");
-scanf("%d",&num);
-new_node= (struct node *)malloc(sizeof(struct node));
-new_node ->data=num;
-new_node ->next=start;
-start=new_node;
-return start;
-};
-
-struct node *display(struct node *start)
+typedef struct node * NODE;
+NODE getnode()
 {
-struct node *ptr;
-ptr = start;
-while(ptr != NULL)
+NODE temp;
+temp=(NODE)malloc(sizeof(struct node));
+if(temp==NULL)
 {
- printf("\t %d", ptr -> data);
- ptr = ptr -> next;
+ printf("memory allocation failed:(\n");
+ return NULL;
 }
-return start;
+return temp;
 }
+NODE insertfront(NODE first,int data)
+{
 
-struct node *revdisplay(struct node *start) {
-   if(start == NULL) {
-      printf("[null] => ");
-      return start;
-   }
+NODE tmp;
+tmp=getnode();
+tmp->info=data;
+tmp->link=first;
+return tmp;
+}
+void display(NODE first)
+{
+NODE cur;
+if(first==NULL)
+{
+printf("empty list");
+return;
+}
+printf("linked list:\n");
+cur=first;
+while(cur!=NULL)
+{
+printf("%d\t",cur->info);
+cur=cur->link;
+}
+}
+NODE reverse(NODE first)
+{
+NODE prev,cur,next;
+if(first==NULL || first->link==NULL)
 
-   revdisplay(start->next);
-   printf(" %d =>",start->data);
+ return first;
+prev=NULL;
+cur=first;
+while(cur!=NULL)
+{
+ next=cur->link;
+ cur->link=prev;
+ prev=cur;
+ cur=next;
 
+}
+return prev;
+}
+void deletebykey(NODE first,int key)
+{
+NODE temp=first,prev;
+if(temp!=NULL && temp->info==key)
+ {
+ first=temp->link;
+ return;
+ }
+ while(temp!=NULL && temp->info!=key)
+ {
+ prev = temp;
+
+ temp = temp->link;
+ }
+ if (temp==NULL)
+ return;
+ prev->link = temp->link;
+free(temp);
+}
+NODE insertpos(NODE first, int data, int pos)
+{
+ NODE tmp=getnode();
+ tmp->info = data;
+ if (first==NULL)
+ {
+ return tmp;
+ }
+ if (pos== 1)
+ {
+ tmp->link =first;
+ return tmp;
+ }
+ NODE cur =first;
+ while (pos-1>0)
+
+ {
+ cur = cur->link;
+ pos--;
+ }
+ tmp->link =cur->link;
+ cur->link =tmp;
+ return first;
+}
+void main()
+{
+NODE first=NULL;
+int ch,item,key,pos;
+for(;;)
+{
+ printf("\n1)-Insert front 2)-Position insert 3)-Display 4)-Reverse List 5)-Delete 6)-exit\n");
+ printf("Enter your choice:");
+ scanf("%d",&ch);
+ switch(ch)
+ {
+ case 1:printf("Enter item to insert:");
+ scanf("%d",&item);
+ first=insertfront(first,item);
+
+ break;
+ case 2:printf("Enter position:");
+ scanf("%d",&pos);
+ printf("Enter item:");
+ scanf("%d",&item);
+ first=insertpos(first,item,pos);
+ break;
+ case 3:display(first);
+ break;
+ case 4:first=reverse(first);
+ break;
+ case 5:printf("Enter key:");
+ scanf("%d",&key);
+ deletebykey(first,key);
+ break;
+ case 6:exit(0);
+ break;
+ }
+}
 }
