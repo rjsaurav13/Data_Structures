7ce5644fb0ab59b0c0530ca17342a307f9c4629f
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2020-11-24 08:18:35 +0530

    Update dcircular.c

--- lab/dcircular.c
+++ lab/dcircular.c
@@ -1,1 +1,199 @@
 
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    struct node *prev;
+    struct node *next;
+    int data;
+};
+struct node *head;
+void insertion_beginning();
+void insertion_last();
+void deletion_beginning();
+void deletion_last();
+void display();
+void search();
+void main ()
+{
+int choice =0;
+    while(choice != 8)
+    {
+        printf("1)-Insert Beginning 2)-Insert last 3)-Delete Beginning 4)-Delete last 5)-Display 6)-Exit\n");
+        printf("Enter your choice?\n");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1:
+                    insertion_beginning();
+                    break;
+            case 2:
+                    insertion_last();
+                    break;
+            case 3:
+                    deletion_beginning();
+                    break;
+            case 4:
+                    deletion_last();
+                    break;
+            case 5:
+                    display();
+                    break;
+            case 6:
+                    exit(0);
+                    break;
+            default:
+                    printf("Please enter valid choice..");
+        }
+    }
+}
+void insertion_beginning()
+{
+   struct node *ptr,*temp;
+   int item;
+   ptr = (struct node *)malloc(sizeof(struct node));
+   if(ptr == NULL)
+   {
+       printf("\nOVERFLOW");
+   }
+   else
+   {
+    printf("\nEnter Item value");
+    scanf("%d",&item);
+    ptr->data=item;
+   if(head==NULL)
+   {
+      head = ptr;
+      ptr -> next = head;
+      ptr -> prev = head;
+   }
+   else
+   {
+       temp = head;
+    while(temp -> next != head)
+    {
+        temp = temp -> next;
+    }
+    temp -> next = ptr;
+    ptr -> prev = temp;
+    head -> prev = ptr;
+    ptr -> next = head;
+    head = ptr;
+   }
+   printf("\nNode inserted\n");
+}
+
+}
+void insertion_last()
+{
+   struct node *ptr,*temp;
+   int item;
+   ptr = (struct node *) malloc(sizeof(struct node));
+   if(ptr == NULL)
+   {
+       printf("\nOVERFLOW");
+   }
+   else
+   {
+       printf("\nEnter value");
+       scanf("%d",&item);
+        ptr->data=item;
+       if(head == NULL)
+       {
+           head = ptr;
+           ptr -> next = head;
+           ptr -> prev = head;
+       }
+       else
+       {
+          temp = head;
+          while(temp->next !=head)
+          {
+              temp = temp->next;
+          }
+          temp->next = ptr;
+          ptr ->prev=temp;
+          head -> prev = ptr;
+      ptr -> next = head;
+        }
+   }
+     printf("\nnode inserted\n");
+}
+
+void deletion_beginning()
+{
+    struct node *temp;
+    if(head == NULL)
+    {
+        printf("\n UNDERFLOW");
+    }
+    else if(head->next == head)
+    {
+        head = NULL;
+        free(head);
+        printf("\nnode deleted\n");
+    }
+    else
+    {
+        temp = head;
+        while(temp -> next != head)
+        {
+            temp = temp -> next;
+        }
+        temp -> next = head -> next;
+        head -> next -> prev = temp;
+        free(head);
+        head = temp -> next;
+    }
+
+}
+void deletion_last()
+{
+    struct node *ptr;
+    if(head == NULL)
+    {
+        printf("\n UNDERFLOW");
+    }
+    else if(head->next == head)
+    {
+        head = NULL;
+        free(head);
+        printf("\nnode deleted\n");
+    }
+    else
+    {
+        ptr = head;
+        if(ptr->next != head)
+        {
+            ptr = ptr -> next;
+        }
+        ptr -> prev -> next = head;
+        head -> prev = ptr -> prev;
+        free(ptr);
+        printf("\nnode deleted\n");
+    }
+}
+
+void display()
+{
+    struct node *ptr;
+    ptr=head;
+    if(head == NULL)
+    {
+        printf("\nnothing to print");
+    }
+    else
+    {
+        printf("\n printing values ... \n");
+
+        while(ptr -> next != head)
+        {
+
+            printf("%d\n", ptr -> data);
+            ptr = ptr -> next;
+        }
+        printf("%d\n", ptr -> data);
+    }
+
+}
+
