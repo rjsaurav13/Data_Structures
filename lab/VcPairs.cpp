ad3be477b5f52ff0704d45a8023b047c096fc380
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2020-12-07 23:13:47 +0530

    Update VcPairs.cpp

--- lab/VcPairs.cpp
+++ lab/VcPairs.cpp
@@ -3,13 +3,24 @@ using namespace std;
 int main(){
 int tcase,len;
 cin>>tcase;
-string word;
+
 for(int i=0;i<tcase;i++)
 {
+    string word;
+    int count=0;
     cin>>len;
     cin>>word;
     word.resize(len);
-    if(word.at(i)=='a'||word.at(i)=='e'||word.at(i)=='i'||word.at(i)=='o'||word.at(i)=='u')
+    for(int i =0;i<len;i++){
+    if((word[i]!='a'&& word[i]!='e'&&word[i]!='i'&&word[i]!='o'&&word[i]!='u')&&(word[i+1]=='a'||word[i+1]=='e'||word[i+1]=='i'||word[i+1]=='o'||word[i+1]=='u'))
+
+    {
+
+        count++;
+
+    }
+    }
+    cout<<count<<"\n";
 
 }
 return 0;
