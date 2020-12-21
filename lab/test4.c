0a8302fb4c0e39460f0a5b90ad7604136dc02500
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2020-12-21 20:19:29 +0530

    Update test4.c

--- lab/test4.c
+++ lab/test4.c
@@ -1,51 +1,39 @@
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
 #include <stdlib.h>
-#define max 4
-int rear=-1,front=0;
-int q[max];
-void insert(){
-    int num;
-if(rear==max-1){
-    printf("Queue full");
+#include <string.h>
+#define max 30
+int top=-1;
+char s[30];
+void push(char item)
+{
+top++;
+s[top]=item;
 }
-else{
-        scanf("%d",num);
-        rear=(rear+1)%max;
-        q[rear]=num;
-        front++;
+char pop()
+{
+char c;
+c=s[top];
+top--;
+return c;
 }
+int main()
+{
+int i,length,count=0;
+char string[30];
+printf("Enter the String:\n");
+gets(string);
+length=strlen(string);
+for(i=0;i<length;i++)
+push(string[i]);
+for(i=0;i<length;i++)
+{
+if(string[i]==pop())
+count++;
 }
-void display(){
-    if(front==0){
-       printf("Queue is empty:");
-       exit(0);
-    }
-    else{
-        for(int i=0;i<rear-1;i++){
-            printf("%d",q[i]);
-        }
-    }
-}
-int main(){
-int ch;
-printf("Enter your choice: \n");
-printf("1)-INSERT 2)-DELETE 3)-DISPLAY 4)-EXIT");
-scanf("%d",&ch);
-switch(ch) {
-case 1:
-    insert();
-    break;
-case 2:
-    printf(":");
-   // del();
-    break;
-case 3:
-    display();
-    break;
-case 4:
-    return;
-    printf("PLEASE CHOOSE CORRECT OPTION:-:");
-}
+printf("%d\n",pop());
+if (count==length)
+printf("Given String is a Palindrome\n");
+else
+printf("Given String is not a Palindrome\n");
 return 0;
 }
