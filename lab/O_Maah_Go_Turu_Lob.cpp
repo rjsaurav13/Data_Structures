c5c5fdfff11cf718719d51a0b0f046aa93e94ca7
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2021-03-16 21:10:03 +0530

    Create O_Maah_Go_Turu_Lob.cpp

---
+++ lab/O_Maah_Go_Turu_Lob.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int tcase;
+  cin>>tcase;
+  for(int j=0;j<tcase;j++){
+    string str;
+    cin>>str;
+    int l=str.length();
+    l=l-1;
+    if() {
+      cout<<"lovely string\n";  
+  }else{
+      cout<<"ugly string\n";
+  }
+  }
+  return 0;
+}
